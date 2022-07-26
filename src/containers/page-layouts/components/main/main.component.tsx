760424755f49c9515c8d5092632a673075ce4e10
jikor1st <jikor1st@naver.com>
2022-07-27 04:56:43 +0900

    fix

--- src/containers/page-layouts/components/main/main.component.tsx
+++ src/containers/page-layouts/components/main/main.component.tsx
@@ -11,20 +11,20 @@ type MainProps = {
 const Main: React.FC<MainProps> = ({ page }) => {
   return (
     <Container styleSet={{ height: PAGE_LAYOUTS_ELEMENT_INFO.header.height }}>
-      <Suspense fallback={<>loading</>}>{page}</Suspense>
+      <Suspense fallback={<LoadingPage />}>{page}</Suspense>
     </Container>
   );
 };
 
-// const LoadingPage = () => {
-//   return (
-//     <LoadingContainer>
-//       <LoadingBox>
-//         <LoadingIndicator />
-//       </LoadingBox>
-//     </LoadingContainer>
-//   );
-// };
+const LoadingPage = () => {
+  return (
+    <LoadingContainer>
+      <LoadingBox>
+        <LoadingIndicator />
+      </LoadingBox>
+    </LoadingContainer>
+  );
+};
 
 const Container = styled.main`
   ${({ styleSet }: { styleSet: { height: number } }) =>
@@ -41,23 +41,22 @@ const LoadingContainer = styled.div`
   position: relative;
 `;
 
-// const Spin = keyframes`
-//   to { transform: rotate(360deg); }
-// `;
-// const LoadingBox = styled.div`
-//   position: absolute;
-//   left: 50%;
-//   top: 35%;
-//   transform: translate(-50%, -50%);
-// `;
-// const LoadingIndicator = styled.div`
-//   display: inline-block;
-//   width: 50px;
-//   height: 50px;
-//   border: 3px solid rgba(255, 255, 255, 0.4);
-//   border-radius: 50%;
-//   border-top-color: #fff;
-//   animation: ${Spin} 1s ease-in-out infinite;
-// `;
-
+const Spin = keyframes`
+  to { transform: rotate(360deg); }
+`;
+const LoadingBox = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 35%;
+  transform: translate(-50%, -50%);
+`;
+const LoadingIndicator = styled.div`
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  border: 3px solid rgba(255, 255, 255, 0.4);
+  border-radius: 50%;
+  border-top-color: #fff;
+  animation: ${Spin} 1s ease-in-out infinite;
+`;
 export { Main };
